d8bdaa09b9312bf1f4c96b26bd818a3f3f8c61dc
Trent <tdcouse@ncsu.edu>
2022-11-18 18:23:04 -0500

    Added test_airbrakes

---
+++ test_airbrakes.py
@@ -0,0 +1,19 @@
+from Servo import Servo
+
+# these angles represent open and closed for the airbrakes, they are arbitrary
+SERVO_OFF_ANGLE = 120
+SERVO_ON_ANGLE = 45
+
+
+# this is the pin that the servo's data wire is plugged into
+SERVO_PIN = 32
+
+servo = Servo(SERVO_PIN, 3.5, 11.5)
+
+print("Type (1) to deploy and (0) to retract the airbrakes.")
+while True:
+    command = input()
+    if command == '0':
+        servo.set_degrees(SERVO_OFF_ANGLE)
+    elif command == '1':
+        servo.set_degrees(SERVO_ON_ANGLE)
