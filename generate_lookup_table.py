3760e207216f586443b98ba64f74d6f5b722b5bf
jgelia <jgelia@ncsu.edu>
2024-03-05 18:50:54 -0500

    Wrote code to generate a lookup table for a bang bang controller

--- generate_lookup_table.py
+++ generate_lookup_table.py
@@ -7,8 +7,9 @@ import time
 
 
 VELOCITY_STEP = 1  # Keep this at 1 for quick look up table indexing
-EXTENSIONS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 FILEPATH = "AirbrakeSystem/lookup_table.csv"
+# This will only be used for when we do apogee estimation for PID control
+EXTENSIONS = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
 
 def get_log_lines(file_path: str = "logs/lookup_table_logs/vel0.0ext0.0.log") -> list[str]:
@@ -23,7 +24,7 @@ def get_control_state_index(lines: list[str]) -> int:
             return i
 
 
-def get_change_in_altitude(lines: list[str], deploy_velocity: float) -> float:
+def get_change_in_altitude(lines: list[str], current_velocity: float) -> float:
     last_altitude = 0
     deploy_altitude = None
     for i in range(0, len(lines)):
@@ -31,7 +32,7 @@ def get_change_in_altitude(lines: list[str], deploy_velocity: float) -> float:
         if parts[1] != "Data point":
             continue
         current_altitude = float(parts[2])
-        if deploy_altitude is None and float(parts[4]) <= deploy_velocity:
+        if deploy_altitude is None and float(parts[4]) <= current_velocity:
             deploy_altitude = current_altitude
         # Checks for reaching apogee
         if current_altitude <= last_altitude:
@@ -78,13 +79,7 @@ def write_lookup_table_to_csv(file_path: str, lookup_table: list):
             writer.writerow([x_value, y_values])
 
 
-def main():
-    # Runs the sim once to get some starting values
-    launch_sim()
-    log_lines = get_log_lines()
-    control_state_index = get_control_state_index(log_lines)
-    max_velocity = float(log_lines[control_state_index + 1].split(",")[4])
-
+def generate_pid_lookup_table(max_velocity: float, control_state_index: int):
     # Makes the skeleton lookup table
     lookup_table = [
         [float(velocity), [[extension, 0.0] for extension in EXTENSIONS]]
@@ -118,7 +113,38 @@ def main():
 
     print(lookup_table)
 
-    write_lookup_table_to_csv("AirbrakeSystem/lookup_table.csv", lookup_table)
+    write_lookup_table_to_csv("AirbrakeSystem/pid_lookup_table.csv", lookup_table)
+
+
+def generate_bang_bang_lookup_table(log_lines: list[str], control_state_index: int):
+    control_lines = log_lines[control_state_index + 1:]
+    lookup_table = []
+    for line in control_lines:
+        if line.split(",")[1] != "Data point":
+            continue
+        velocity = float(line.split(",")[4])
+        if velocity > 0:
+            lookup_table.append([velocity, get_change_in_altitude(control_lines, velocity)])
+        else:
+            break
+
+        print(lookup_table)
+
+        write_lookup_table_to_csv("AirbrakeSystem/bang_bang_lookup_table.csv", lookup_table)
+
+
+def main():
+    # Runs the sim once to get some starting values
+    launch_sim()
+    log_lines = get_log_lines()
+    control_state_index = get_control_state_index(log_lines)
+    max_velocity = float(log_lines[control_state_index + 1].split(",")[4])
+
+    # This took my computer about 30 minutes
+    # generate_pid_lookup_table(max_velocity, control_state_index)
+
+    # This took my computer 5 seconds
+    generate_bang_bang_lookup_table(log_lines, control_state_index)
 
 
 if __name__ == "__main__":
