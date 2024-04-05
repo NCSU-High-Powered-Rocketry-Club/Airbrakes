ffc419f16b0463128cb13786f1713d39add71250
Graicc <33105645+Graicc@users.noreply.github.com>
2024-04-05 13:50:31 -0400

    Use true multiprocess for reading from interface

--- AirbrakeSystem/hardware/MSCLInterface.py
+++ AirbrakeSystem/hardware/MSCLInterface.py
@@ -6,6 +6,8 @@ from collections import deque
 from ..data import ABDataPoint
 import mscl
 
+from multiprocessing import Process, Pipe, Value
+
 # TOOD (Before every launch): Make sure this value is correct
 UPSIDE_DOWN = True
 
@@ -16,25 +18,26 @@ class MSCLInterface:
     Parker-LORD 3DMCX5-AR.
     """
 
-    def __init__(self, port, raw_data_logfile: TextIOWrapper, est_data_logfile: TextIOWrapper):
+    def __init__(
+        self, port, raw_data_logfile: TextIOWrapper, est_data_logfile: TextIOWrapper
+    ):
         # creating data node
         self.connection = mscl.Connection.Serial(port)
         self.node = mscl.InertialNode(self.connection)
         self.raw_data_logfile = raw_data_logfile
         self.est_data_logfile = est_data_logfile
 
-        self.databuffer = deque()
-
-        self.running = False
+        self.parent_pipe, self.child_pipe = Pipe()
+        self.running = Value("b", False)
 
         # rate in which we poll date  in miliseconds (1/(Hz)*1000)
-        self.polling_rate = int(1/(100)*1000)
+        self.polling_rate = int(1 / (100) * 1000)
 
         self.last_time: int = 0
 
     def stop_logging_loop(self):
-        """ Stops the logging loop. """
-        self.running = False
+        """Stops the logging loop."""
+        self.running.value = False
         self.logging_thread.join()
         self.raw_data_logfile.close()
         self.est_data_logfile.close()
@@ -44,41 +47,40 @@ class MSCLInterface:
         Using the threading package to create a logging loop on a separate
         thread.
         """
-        self.logging_thread = threading.Thread(target=self.start_logging_loop)
+        self.logging_thread = Process(target=self.start_logging_loop))
         self.logging_thread.start()
 
     def start_logging_loop(self):
         """
         Method used to run the logging loop until stopped by
-        `stop_logging_loop`. All data collected by the IMU is logged to the 
+        `stop_logging_loop`. All data collected by the IMU is logged to the
         logfile. All the accelerating and time data is saved to the data
         buffer.
         """
-        self.running = True
+        self.running.value = True
         counter = 0
 
         have_raw = False
         have_est = False
-        while self.running:
+        while self.running.value:
             # get all the data packets from the node with a timeout of the
             # polling rate in milliseconds
-            packets: mscl.MipDataPackets = self.node.getDataPackets(
-                self.polling_rate)
+            packets: mscl.MipDataPackets = self.node.getDataPackets(self.polling_rate)
 
             packet: mscl.MipDataPacket
             for packet in packets:
                 # Log the headers to a file if we haven't already
-                if (not have_raw):
+                if not have_raw:
                     data_point: mscl.MipDataPoint
                     for data_point in packet.data():
-                        if (data_point.channelName()[:3] != "est"):
+                        if data_point.channelName()[:3] != "est":
                             self.print_headers(packet, self.raw_data_logfile)
                             have_raw = True
                         break
 
-                if (not have_est):
+                if not have_est:
                     for data_point in packet.data():
-                        if (data_point.channelName()[:3] == "est"):
+                        if data_point.channelName()[:3] == "est":
                             self.print_headers(packet, self.est_data_logfile)
                             have_est = True
                         break
@@ -92,14 +94,14 @@ class MSCLInterface:
 
     def print_headers(self, packet, logfile):
         for data_point in packet.data():
-            logfile.write(str(data_point.channelName())+",")
+            logfile.write(str(data_point.channelName()) + ",")
         logfile.write("\n")
 
     def pop_data_point(self) -> ABDataPoint | None:
-        """Pops a data point off of the left of the databuffer deque"""
+        """Pops a data point off of the databuffer pipe"""
         try:
             # TODO: This should always return a data point with all felids filled
-            ret: ABDataPoint = self.databuffer.popleft()
+            ret: ABDataPoint = self.parent_pipe.recv()
             self.last_time = ret.timestamp
             return ret
         except IndexError:
@@ -139,7 +141,7 @@ class MSCLInterface:
             # if the data object is not empty
             if contains_data:
                 # send the processed data to the databuffer
-                self.databuffer.append(data_object)
+                self.child_pipe.send(data_object)
 
-            logfile.write(str(data_point.as_float())+",")
+            logfile.write(str(data_point.as_float()) + ",")
         logfile.write("\n")
